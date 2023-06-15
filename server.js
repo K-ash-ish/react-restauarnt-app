13728cc7db8258c4ef6a66394686cda89d3932df
K-ash-ish <kashishsondhiya@gmail.com>
2023-06-15 16:04:16 +0530

    updating swiggy link

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const app = express();
 app.use(cors());
 
 app.get("/api/restaurants", (req, res) => {
-  const { latitude, longitude } = req.query;
+  const { latitude, longitude, offset } = req.query;
   // 23.1768293 79.97640129999999
   //asdf
   // https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.1768293&lng=79.97640129999999&page_type=DESKTOP_WEB_LISTING
-  const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${latitude}&lng=${longitude}&page_type=DESKTOP_WEB_LISTING`;
+  const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${latitude}&lng=${longitude}&offset=${offset}&sortBy=RELEVANCE&page_type=DESKTOP_WEB_LISTING`;
 
   fetch(url, {
     headers: {
